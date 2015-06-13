09ebd95291340058723716a2795777e85ba81e22
Lolleko <larry.junglas@hotmail.de>
2015-06-13 17:16:13 +0200

    testing with player table

--- gamemode/shared.lua
+++ gamemode/shared.lua
@@ -9,6 +9,7 @@ include( "modules/team.lua" )
 include( "sh_warriors.lua")
 include( "player_classes/player_test.lua" )
 include( "screen_effects.lua" )
+include( "player_ext.lua" )
 
 TEAM_BLUE = 1
 
